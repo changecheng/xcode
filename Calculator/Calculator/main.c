2494433e43657b855849c5fbf95234041cec48c1
changecheng <ChangeCheng@192.168.0.6>
2014-08-17 18:48:02 +0800

    calculator

--- Calculator/Calculator/main.c
+++ Calculator/Calculator/main.c
@@ -14,40 +14,40 @@ typedef struct {
     int length;
     
 } Stack;
-void initStack(Stack st){
-    st.length=0;
+void initStack(Stack *st){
+    st->length=0;
 }
-int emptyStack(Stack st){
-    if (st.length==0) {
+int emptyStack(Stack *st){
+    if (st->length==0) {
         return 1;
     }
     return 0;
 }
-int fullStack(Stack st) {
-    if (st.length==Max) {
+int fullStack(Stack *st) {
+    if (st->length==Max) {
         return 1;
     }
     return 0;
 }
-int pop(Stack st){
+int pop(Stack *st){
     
     if (emptyStack(st)) {
         printf("Stack is empty!\n");
         return 0;
     }
-    st.length--;
-    return st.data[st.length];
+    st->length--;
+    return st->data[st->length];
 }
-void push(Stack st,int n) {
-    st.data[st.length]=n;
-    st.length++;
+void push(Stack *st,int n) {
+    st->data[st->length]=n;
+    st->length++;
     
 }
-int topStack(Stack st){
-    return st.data[st.length-1];
+int topStack(Stack *st){
+    return st->data[st->length-1];
 }
-void clearStack(Stack st){
-    for (int i=0; i<st.length; i++) {
+void clearStack(Stack *st){
+    for (int i=0; i<st->length; i++) {
         pop(st);
     }
 }
@@ -63,21 +63,74 @@ int isNum(int c){
     }
     return 0;
 }
-void ProcessString(int array[]){
-    int c;
+//process operation expression
+void ProcessString(int array[],int opOrNum[]){
+//    int opOrNum[Max];//store tag of array
+    int lastisop=0;
+    int lastop=0;  //store last op
+    int c=0;
+    int lessThanZeroNum=0;
+    int x=0;//num temp
+    int tag=0;  //record whether there is a number input
     int i=0;
     while ((c=getchar())!='\n') {
         //process char
         if (isNum(c)) {
             c=c-48;
-            array[i]=c;
-            i++;
+            //consider whether is num under zero
+            if (lessThanZeroNum==1) {
+                x=10*x-c;
+            }else{
+                x=10*x+c;
+            }
+            tag=1;
+//            array[i]=c;
+//            i++;
         }else if (isOperator(c)){
-            array[i]=c;
-            i++;
+            if (tag==1) {         //encounter an op,last input is a number
+                array[i]=x;
+                opOrNum[i]=0;    //0 means num
+                lastisop=0;
+                lessThanZeroNum=0;
+                x=0;
+                tag=0;
+                i++;
+            }
+        //consider unary -
+            if (lastisop==1 && c==45 && lastop!=41) {   //last op should not be ')'
+                //unary -
+                lessThanZeroNum=1;
+                
+            }else{
+                //not unary - normal operation
+                array[i]=c;
+                opOrNum[i]=1;       // 1 means op
+                i++;
+                lastisop=1;
+                lastop=c;
+            }
+            
+            
         }
     }
-    array[i]=35; //add # to end
+    if (tag==1) {
+        array[i]=x;
+        opOrNum[i]=0;
+        i++;
+        array[i]=35; //add # to end
+        opOrNum[i]=1;
+    }else{
+        array[i]=35; //add # to end
+        opOrNum[i]=1;
+
+    }
+    
+//    if (x!=0) {
+//        array[i]=x;
+//        opOrNum[i]=0;
+//        i++;
+//    }
+    
 }
 int dect(int c){
     switch (c) {
@@ -108,8 +161,45 @@ int dect(int c){
     return 0;
 }
 
+char convertOp(int c){
+    switch (c) {
+        case 43:
+            return '+';
+            break;
+        case 45:
+            return '-';
+            break;
+        case 42:
+            return '*';
+            break;
+        case 47:
+            return '/';
+            break;
+        case 40:
+            return '(';
+            break;
+        case 41:
+            return ')';
+            break;
+        case 35:
+            return '#';
+            break;
+        default:
+            break;
+    }
+   return '?';
+
+}
+/////main
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    printf("@@@@@@@@@@ @@@@@@@ @@@   @@   @@    @@@@  \n");
+    printf("      @@@  @@      @@@@  @@  @@@   @@   @ \n");
+    printf("    @@@    @@@@@@  @@ @@ @@   @@     @@   \n");
+    printf("  @@@      @@      @@  @@@@   @@   @   @@ \n");
+    printf("@@@@@@@@@@ @@@@@@@ @@   @@@  @@@@   @@@@  \n");
+    
+    printf("Simple Calculator     made by Zen1s\n");
+    printf("Please input your calculation expression: \n");
     
     // priority
     int priority[7][7]={
@@ -121,69 +211,124 @@ int main(int argc, const char * argv[]) {
         {1,1,0,0,1,1,1},
         {1,1,1,1,1,-1,1},
         {0,0,0,0,-1,2,1},
-        {0,0,0,0,1,0,-1}
+        {0,0,0,0,1,0,3}
     
     };
     //conver symbol to choose priority
 //    int n=dect(35);
 //    printf("%d",n);
-    int array[Max];
-    int tempnum1,tempnum2;
-    ProcessString(array);    //convert operation to number array
-    for (int i=0; i<32; i++) {
-        printf("%d,",array[i]);
-    }
+    int array[Max],opOrNum[Max];
+    int tempnum1,tempnum2,tempsum=0,tempop=0;
+    ProcessString(array,opOrNum);    //convert operation to number array
+    //Print opOrNum and array////////////////
+//    for (int i=0; i<Max-1; i++) {
+//        printf(" %d ",opOrNum[i]);
+//    }
+//    printf("\n");
+//    for (int i=0; i<Max-1; i++) {
+//        printf("%d,",array[i]);
+//    }
     //execute
     Stack operands,operators;
-    push(operators, 35);//initialization
-    for (int i=0; i<Max; i++) {
-        if (array[i]>=0&&array[i]<=9) {
+    push(&operators, 35);//initialization
+    int unfinished = 1;
+    int errorTag=0;
+    printf("\n");
+    
+    for (int i=0; i<Max&&unfinished; i++) {
+        if (opOrNum[i]==0) {              //array[i]>=0&&array[i]<=9
             //num into operands
-            push(operands, array[i]);
+            push(&operands, array[i]);
         }else{
             //symbols into operators
-            int lastop=topStack(operators);
+            int lastop=topStack(&operators);
             int prio=priority[dect(array[i])][dect(lastop)];
+           // printf("prio is: %d \n",prio);
             switch (prio) {
                 case 1:
-                    push(operators, array[i]);
+                    push(&operators, array[i]);
                     break;
                 case 0:
                     //priority less than last operator
+                    if (operands.length<2) {  //operands less than 2, error
+                        printf("Error, you operation expression is wrong!!\n");
+                        unfinished=0;
+                        errorTag=1;
+                    }else{
+                        tempnum2 = pop(&operands);
+                        tempnum1 = pop(&operands);
+                        tempop=pop(&operators);
+                        //printf("temp op is %d\n",tempop);
+                        switch (tempop) {
+                            case 43:
+                                tempsum = tempnum1 + tempnum2;   //+
+                                break;
+                            case 45:
+                                tempsum = tempnum1 - tempnum2;  //-
+                                break;
+                            case 42:
+                                tempsum = tempnum1 * tempnum2;  //*
+                                break;
+                            case 47:
+                                if (tempnum2==0) {
+                                    //error
+                                    printf("Error, 0 can't be divided!!\n");
+                                    unfinished=0;
+                                    errorTag=1;
+                                }else{
+                                    tempsum = tempnum1 / tempnum2;
+                                }
+                                // /
+                                break;
+                                
+                            default:
+                                break;
+                        }
+//                        printf("temp sum is %d\n",tempsum);
+                        push(&operands,tempsum);
+                        i--;
+
+                    }
                     break;
                 case -1:
-                    //finish
+                    // ==
+                    pop(&operators);
+//                    unfinished=0;
                     break;
+                case 3:
+                    //finished
+                    unfinished=0;
+                    break;   //fcuk!! It cost me so much time to find this bug.
                 case 2:
                     //error
-                    printf("Your operation is wrong!\n");
+                    printf("Your operation expression is wrong!\n");
+                    errorTag=1;
                     break;
+                
                 default:
                     break;
             }
-//            if (prio==1) {
-//                push(operators, array[i]);
-//            }else if (prio ==0){
-//                //priority less than last operator
-//            }else if () {
-//                
-//            }
-//                
-//            }
+        ///
+   
+            
         }
+        //Following is used to print stacks
+//        for (int i = Max-1; i>=0; i--) {
+//            printf("%d-",operators.data[i]);
+//        }
+//        printf("operators\n");
+//        
+//        for (int i = Max-1; i>=0; i--) {
+//            printf("%d+",operands.data[i]);
+//        }
+//        printf("operands\n");
+    }
+    if (errorTag==0) {
+        printf("The result is %d.\n",pop(&operands));
     }
     
     
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    printf("Hello, World!\n");
     return 0;
 }
