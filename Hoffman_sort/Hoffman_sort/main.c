f2e9fe94dfc2e43ee8b136fef0ae7b02297a7e5f
changecheng <ChangeCheng@192.168.0.6>
2014-08-19 12:48:49 +0800

    cc

--- Hoffman_sort/Hoffman_sort/main.c
+++ Hoffman_sort/Hoffman_sort/main.c
@@ -7,61 +7,156 @@
 //
 
 #include <stdio.h>
-//const int num = 32;
-//void Swap(int *x,int *y){
-//    int temp;
-//    temp = *x;
-//    *x=*y;
-//    *y=temp;
-//    
-//}
-//void BubbleSort(int array[]){
-//    for (int i=0; i<num-1; i++) {
-//        for (int j=0; j<num-1-i; j++) {
-//            //compare array[j] and array[j+1];
-//            if (array[j]>array[j+1]) {
-//                Swap(&array[j],&array[j+1]);
-//            }
-//        }
-//    }
-//}
+
 const int MaxBit=16;
+const int num=5;
 typedef struct {
     int bit[MaxBit];
     int start;
     
 }HCodeType;
-
+//define a stack
 typedef struct {
-    char name;
+    int data[MaxBit];
+    int length;
+}Stack;
+void initStack(Stack *st){
+    st->length=0;
+}
+int emptyStack(Stack *st){
+    if (st->length==0) {
+        return 1;
+    }
+    return 0;
+}
+int fullStack(Stack *st) {
+    if (st->length==MaxBit) {
+        return 1;
+    }
+    return 0;
+}
+int pop(Stack *st){
+    
+    if (emptyStack(st)) {
+        printf("Stack is empty!\n");
+        return 0;
+    }
+    st->length--;
+    return st->data[st->length];
+}
+void push(Stack *st,int n) {
+    st->data[st->length]=n;
+    st->length++;
+    
+}
+int topStack(Stack *st){
+    return st->data[st->length-1];
+}
+void clearStack(Stack *st){
+    for (int i=0; i<st->length; i++) {
+        pop(st);
+    }
+}
+typedef struct {
+    
     int weight;
     int parent;
     int lchild;
     int rchild;
     
 }HNodeType;  // def HNode
-void initHNode(HNodeType *HuffNode,char name,int weight){
-    HuffNode->name = name;
-    HuffNode->weight= weight;
-    HuffNode->parent=-1;
-    HuffNode->lchild=-1;
-    HuffNode->rchild=-1;
-    
-    
-}
 int main(int argc, const char * argv[]) {
-    HNodeType *HuffNodeArray[5];
-    initHNode(HuffNodeArray[0],'A',5);
-    initHNode(HuffNodeArray[1],'B',4);
-    initHNode(HuffNodeArray[2],'C',3);
-    initHNode(HuffNodeArray[3],'D',2);
-    initHNode(HuffNodeArray[4],'E',1);
-    for (int i=0; i<5; i++) {
-       // printf("%c",HuffNodeArray[i].name);
-        printf("%d,%d,%d,%d\n",HuffNodeArray[i]->weight,HuffNodeArray[i]->parent,HuffNodeArray[i]->lchild,HuffNodeArray[i]->rchild);
+    HNodeType HuffmanTree[2*num-1];
+    //symbols and weight
+    int weight[]={5,4,3,2,1};
+    char name[]={'A','B','C','D','E'};
+    //initialize HuffmanTree
+//init 0~num-1
+    for (int i=0; i<=num-1; i++) {
+        HuffmanTree[i].weight= weight[i];
+        HuffmanTree[i].parent=-1;
+        HuffmanTree[i].lchild=-1;
+        HuffmanTree[i].rchild=-1;
+    }
+//init num~2*num-1
+    for (int i=num; i<2*num-1; i++) {
+        HuffmanTree[i].weight= 0;
+        HuffmanTree[i].parent=-1;
+        HuffmanTree[i].lchild=-1;
+        HuffmanTree[i].rchild=-1;
     }
     
+//  print weight for test //
     
-    printf("Hello, World!\n");
+//    for (int i=0; i<2*num-1; i++) {
+//        printf("weight is %d\n",HuffmanTree[i].weight);
+//    }
+    int i=0;
+    int h1,h2;
+    for (i=num; i<2*num-1; i++) {
+//select two lowest weight node
+        h1=0,h2=0;
+        int m1=1000;
+        int m2=1000;
+        for (int j=0; j<=i-1; j++) {
+            
+            //select two node having lowest weight and no parent node
+            if (HuffmanTree[j].parent==-1) {
+//                printf("j is %d\n",j);
+                if (HuffmanTree[j].weight<m1) {
+                    m2=m1;
+                    h2=h1;
+                    
+                    h1=j;
+                    m1=HuffmanTree[j].weight;
+//                    printf("m1 is %d\n",m1);
+    
+                }else if(HuffmanTree[j].weight<m2){
+                    h2=j;
+                    m2=HuffmanTree[j].weight;
+                }
+            }
+        }
+
+//        printf("h1,h2 are %d,%d.\n",h1,h2);
+//Update changed nodes//
+        HuffmanTree[h1].parent=i;
+        HuffmanTree[h2].parent=i;
+        HuffmanTree[i].weight=HuffmanTree[h1].weight+HuffmanTree[h2].weight;
+        HuffmanTree[h1].weight=0;   //lchild weight =0
+        HuffmanTree[h2].weight=1;   //rchild weight =1
+        HuffmanTree[i].lchild=h1;
+        HuffmanTree[i].rchild=h2;
+        
+        
+    }
+
+    Stack code;   //code stack, store inversed code
+    initStack(&code);
+    
+    for (int i=0; i<num; i++) {
+        printf("Code of %c: ",name[i]);
+        int j=i;
+        
+        while (HuffmanTree[j].parent!=-1) {
+
+            push(&code, HuffmanTree[j].weight);
+            j=HuffmanTree[j].parent;
+        }
+        //print code;
+        while (!emptyStack(&code)) {
+            printf("%d",pop(&code));
+        }
+        
+        printf("\n");
+    }
+    
     return 0;
 }
+
+
+
+
+
+
+
---
+++ Quick_Sort and Binary_Search/Quick_Sort and Binary_Search/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Quick_Sort and Binary_Search
+//
+//  Created by Change Cheng on 14/8/19.
+//  Copyright (c) 2014年 Change Cheng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
